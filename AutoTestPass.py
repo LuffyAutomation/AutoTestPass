7d72f6aea61d4478dd14e7dc53692a1657200afe
Luffy <huangfei01@beyondsoft.com>
2017-05-08 10:34:57 -0700

    uuu

--- AutoTestPass.py
+++ AutoTestPass.py
@@ -3,10 +3,21 @@
 from fwk.utils.utilNetwork.UtilNetwork import UtilNetwork
 from ui.flask.uiPortal import app
 import sys
+import webbrowser
 reload(sys)
 
 sys.setdefaultencoding('utf8')
-if __name__ == '__main__':
+
+def startServer(*args):
     # app.run(debug=True)
-    utilNetwork = UtilNetwork()
-    app.run(host=utilNetwork.get_local_ip(), port=8080)
+    app.run(host=args[0], port=args[1])
+
+def openUrl(*args):
+    webbrowser.open("http://" +args[0] + ":" + args[1])
+
+if __name__ == '__main__':
+    _UtilNetwork = UtilNetwork()
+    local_ip = _UtilNetwork.get_local_ip()
+    local_port = "8080"
+    webbrowser.open("http://" + local_ip + ":" + local_port)
+    app.run(host=local_ip, port=local_port)
