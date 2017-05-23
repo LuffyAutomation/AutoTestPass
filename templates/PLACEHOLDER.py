47870fbb87945389cc2c539af4167300c0a858a7
Luffy <huangfei01@beyondsoft.com>
2017-05-23 15:32:08 -0700

    UUUU

--- templates/PLACEHOLDER.py
+++ templates/PLACEHOLDER.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 if __name__ == '__main__':
     suite = unittest.TestSuite()
 
-    suite.addTest(PLACEHOLDER("test_your_flow"))
+    suite.addTest(PLACEHOLDER("test_flow"))
     suite.addTest(PLACEHOLDER("test_your_other_flow"))
 
     test_result = unittest.TextTestRunner(verbosity=2).run(suite)
